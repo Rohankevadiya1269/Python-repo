c887f843cc74631efcbe6dc74c8bd627f7c0ea5e
Rohan <kevadiyarohan64@gmail.com>
2023-05-10 12:25:57 +0530

    Variables in python

---
+++ var.py
@@ -0,0 +1,10 @@
+# variables are like empty containers that stores some values. It basically holds data
+a=1234
+print(a)
+print()
+name="Rohan"
+print(name)
+print()
+b=True
+print(type(b))# Type keyword is used to get the type of the data
+
