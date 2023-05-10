832195b12884fd93a8d5717da1583586a84c581e
Rohan <kevadiyarohan64@gmail.com>
2023-05-10 12:04:53 +0530

    escape char

--- helloWorld.py
+++ helloWorld.py
@@ -10,3 +10,13 @@ print()
 print(15*2)
 """To add
 multi-line comment we can use triple quotes"""
+
+print("Twinkle Twinkle Little star\n How I wonder what you are \nup above the world so high \nlike a diamond in the sky")
+# to print a single strting in multiple line we can use escape characters
+# here we've used \n which is used to print string in new line
+print()
+print("Author:\t Digvijay Sinha") # \t will print tab after author name
+print()
+print("Raj said\"I am a good boy\" ") # \" is used to enter double quote inside double quote
+print()
+print('I am Raj\'s best-friend') # \' is used to enter single quote inside single quote
