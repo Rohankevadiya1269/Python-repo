c008086fd45e527c7a5cffb4d2cabb2b8115d167
Rohan <kevadiyarohan64@gmail.com>
2023-05-10 11:50:09 +0530

    Hello World

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+print("Hello World!") # this will print Hello World
+print() #This will print an empty line
+
+print("""Twinkle Twinkle Little star
+How I wonder what you are
+up above the world so high 
+like a diamond in the sky""") # to print multi line string we use triple quotes
+
+print()
+print(15*2)
+"""To add
+multi-line comment we can use triple quotes"""
