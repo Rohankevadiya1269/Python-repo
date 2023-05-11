9baf0fc352eba3ea732e89e0632e8dba1cda790c
Rohan <kevadiyarohan64@gmail.com>
2023-05-11 12:35:57 +0530

    Exercise codes of for loop statement

---
+++ for-loop.py
@@ -0,0 +1,19 @@
+ # use of for loop
+Colors=["Red", "Yellow", "Blue", "Green", "Black", "White"]
+for color in Colors:
+    print(color)
+    
+    # Using steps in for loop
+for num1 in range(0,36,5): # The range function shows starting element ranging to ending element and the step
+    print(num1)
+    
+#     Printing Table of 13 using for loop
+for f in range(1,11,1):
+       k= f*13
+       print("13 x",f,"=",k)
+       
+       
+#     Printing Table of 27 using for loop
+for f in range(1,11,1):
+       k= f*27
+       print("27 x",f,"=",k)
